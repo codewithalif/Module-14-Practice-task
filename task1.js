3e58790c7f01b6cef2b95f43c4207cc06a115728
codewithalif <programmingwithalif@gmail.com>
2026-02-08 21:28:19 +0600

    task1 is complete

---
+++ task1.js
@@ -0,0 +1,9 @@
+const burgerPrice = 600;
+const cokePrice = 30;
+
+if(burgerPrice > 500){
+  console.log('you can take coke free ');
+}
+else{
+  console.log('coke are not free you can pay ' + cokePrice);
+}
