2de6d3ceb8178aff0a0eb82775a7f951dfdd4300
codewithalif <programmingwithalif@gmail.com>
2026-02-08 21:51:37 +0600

    task 3 is complete

---
+++ Task3.js
@@ -0,0 +1,23 @@
+const Yourmark = 91;
+
+if(Yourmark >= 90 && Yourmark <= 100){
+  console.log('You get Golden A+')
+}
+else if(Yourmark >= 80){
+  console.log('You get Simple A+')
+}
+else if(Yourmark >= 70){
+  console.log('You get A-')
+}
+else if (Yourmark >= 60){
+  console.log('You get B')
+}
+else if (Yourmark >= 50){
+  console.log('You get B')
+}
+else if(Yourmark >= 33){
+  console.log('You get B- ')
+}
+else{
+  console.log('You are failed')
+}
