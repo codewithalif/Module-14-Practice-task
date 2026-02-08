ee7ca0720a339f4c51039900395f892482c2f4e3
codewithalif <programmingwithalif@gmail.com>
2026-02-08 22:38:53 +0600

    task 5 some issur fixed

--- Task5.js
+++ Task5.js
@@ -1,15 +1,16 @@
 const num1 = 50;
 const num2 = 65;
 let result = 100;
+// simple if else condition
 
-// if(num1 > num2){
-//   result = result * 2;
-// }
-// else{
-//   result = num1 + num2;
-// }
-// console.log(result);
+if(num1 > num2){
+  result = result * 2;
+}
+else{
+  result = num1 + num2;
+}
+console.log(result);
 
 // Now write ternary way
 
-num1 > num2 ? result = result * 2 : result = num1 + num2
+num1 > num2 ? result = result * 2 : result = num1 + num2;
